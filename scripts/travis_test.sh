49a7d1f19230272f15e8e6f80ade745c67dee698
JP Smith <jp.smith@trailofbits.com>
2017-06-28 18:05:12 -0400

    make sure the python script returns 0

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -3,9 +3,11 @@
 RV=0
 
 # Google 2016 Unbreakable
-cd google2016_unbreakable
 time python win.py | tee unbreakable.log
-if grep -q "CTF{0The1Quick2Brown3Fox4Jumped5Over6The7Lazy8Fox9}" unbreakable.log
+RAN_OK=${PIPESTATUS[0]}
+grep -q "CTF{0The1Quick2Brown3Fox4Jumped5Over6The7Lazy8Fox9}" unbreakable.log
+GOT_FLAG=$?
+if [[ $RAN_OK && $GOT_FLAG ]]
 then
     echo "Google 2016 Unbreakable passed"
 else
@@ -17,7 +19,10 @@ cd ..
 # Manticore Challenge
 cd manticore_challenge
 time python win.py | tee mcore_challenge.log
-if grep -q "=MANTICORE==" mcore_challenge.log
+RAN_OK=${PIPESTATUS[0]}
+grep -q "=MANTICORE==" mcore_challenge.log
+GOT_FLAG=$?
+if [[ $RAN_OK && $GOT_FLAG ]]
 then
     echo "Manticore Challenge passed"
 else
