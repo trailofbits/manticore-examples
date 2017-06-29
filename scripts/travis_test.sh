14a7739e634d1001807ea323737ffc4c7dabcda1
JP Smith <jp.smith@trailofbits.com>
2017-06-29 13:10:09 -0400

    use pipestatus -o

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
+set -o pipefail
 
 RV=0
 
 # Google 2016 Unbreakable
 cd google2016_unbreakable
-time python win.py | tee unbreakable.log
-RAN_OK=${PIPESTATUS[0]}
-grep -q "CTF{0The1Quick2Brown3Fox4Jumped5Over6The7Lazy8Fox9}" unbreakable.log
-GOT_FLAG=$?
-if [[ $RAN_OK -eq 0 && $GOT_FLAG -eq 0 ]]
+FAILED=0
+time python win.py | tee unbreakable.log || FAILED=1
+grep -q "CTF{0The1Quick2Brown3Fox4Jumped5Over6The7Lazy8Fox9}" unbreakable.log || FAILED=1
+if [[ $FAILED -eq 0 ]]
 then
     echo "Google 2016 Unbreakable passed"
 else
@@ -19,11 +19,10 @@ cd ..
 
 # Manticore Challenge
 cd manticore_challenge
-time python win.py | tee mcore_challenge.log
-RAN_OK=${PIPESTATUS[0]}
-grep -q "=MANTICORE==" mcore_challenge.log
-GOT_FLAG=$?
-if [[ $RAN_OK -eq 0 && $GOT_FLAG -eq 0 ]]
+FAILED=0
+time python win.py | tee mcore_challenge.log || FAILED=1
+grep -q "=MANTICORE==" mcore_challenge.log || FAILED=1
+if [[ $FAILED -eq 0 ]]
 then
     echo "Manticore Challenge passed"
 else
