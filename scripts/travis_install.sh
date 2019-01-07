5ecfa7d2ca33f44b4030d0a08910276d668af3f8
disconnect3d <dominik.b.czarnota@gmail.com>
2019-01-07 08:54:31 -0800

    Install native!

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -2,5 +2,5 @@ pip uninstall -y Manticore || echo "Manticore not cached"
 git clone https://github.com/trailofbits/manticore.git
 git --no-pager log -1
 cd manticore
-pip install .
+pip install .[native]
 cd ..
